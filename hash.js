6772b161ad390dab07ee28b7ec94d2c4504b7e2f
Gaby Nieva <gaby.nieva@outlook.com>
2023-05-27 21:20:16 -0600

    HashTable - métodos get, getKeys y delete

--- hash.js
+++ hash.js
@@ -20,11 +20,57 @@ class HashTable {
         this.data[address].push([key, value]);
         return this.data;
     }
+
+    get(key){
+        const address = this.hashMethod(key);
+        const currentBucket = this.data[address];
+        if ( currentBucket ) {
+            for ( let i = 0; i < currentBucket.length; i++){
+                if ( currentBucket[i][0] === key ){
+                    return currentBucket[i][1];
+                }
+            }
+        }
+        return undefined;
+    }
+
+    delete(key){
+        const address = this.hashMethod(key);
+        const currentBucket = this.data[address];
+        if ( currentBucket ) {
+            for ( let i = 0; i < currentBucket.length; i++){
+                if ( currentBucket[i][0] === key ){
+                    const element =  currentBucket[i][1];
+                    currentBucket.splice(i, 1);
+                    return element;
+                }
+            }
+        }
+        return undefined;
+    }
+
+    getKeys(){
+        const keys = [];
+        this.data.forEach( (bucket) => {
+            bucket.forEach( (keyPairValues) => {
+                keys.push(keyPairValues[0]);
+            });
+        });
+        return keys;
+    }
 }
 
 // generar 50 buckets
 const myHashTable = new HashTable(50);
+
 myHashTable.set("Ginger", 2013);
 myHashTable.set("KC", 2016);
 myHashTable.set("Moji", 2021);
+myHashTable.set("Nicky", 1995);
+myHashTable.set("Blackie", 2003);
+myHashTable.set("Candy", 1982);
 console.log(myHashTable);
+console.log(myHashTable.get("Ginger"));
+console.log(myHashTable.getKeys());
+myHashTable.delete('Moji');
+console.log(myHashTable.getKeys());
